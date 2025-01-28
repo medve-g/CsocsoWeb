bb5abde2859f8382dabe6244a59c35eacd293c64
medvegergo <medvegergo@gszi.edu.hu>
2025-01-28 18:04:11 +0100

    Basic LogInSignIn component set up

---
+++ CsocsoCode/src/components/LogInSignUp/LogInSignUp.jsx
@@ -0,0 +1,20 @@
+function LogInSignUp() {
+    return ( 
+        <div className="container">
+            <div className="header">
+                <div className="text">Log In</div>
+            </div>
+            <div className="inputs">
+                <div className="fields">
+
+                </div>
+                <div className="submit">
+
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default LogInSignUp;
