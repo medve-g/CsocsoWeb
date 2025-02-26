43e6c672aaa732e933497f188ee07b2731870d6d
Varga Dávid <vargadavid050123@gmail.com>
2025-02-26 07:55:49 +0100

    News base and slider done

--- CsocsoCode/src/components/pages/WelcomePage.jsx
+++ CsocsoCode/src/components/pages/WelcomePage.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 import kep1 from "../../../public/kep1.jpg";
 import kep2 from "../../../public/kep2.jpg";
@@ -8,88 +10,89 @@ import kep4 from "../../../public/kep4.jpg";
 import kep5 from "../../../public/kep5.jpg";
 import logo from "../../../public/logo.png";
 
+// Slider arrow customization
+function NextArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: 'green-100' }}
+            onClick={onClick}
+        />
+    );
+}
+
+function PrevArrow(props) {
+    const { className, style, onClick } = props;
+    return (
+        <div
+            className={className}
+            style={{ ...style, display: "block", background: 'green-100' }}
+            onClick={onClick}
+        />
+    );
+}
 
 export function WelcomePage() {
+    // Slider settings
+    let SliderSettings = {
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        
+        nextArrow: <NextArrow />,
+        prevArrow: <PrevArrow />
+    };
 
-    let [slideShowPicture,setSlideShowPicture] = useState(kep1);
-    let [pictureCounter,setPictureCounter] = useState(1);
+    let [slideShowPicture, setSlideShowPicture] = useState(kep1);
+    let [pictureCounter, setPictureCounter] = useState(1);
 
-
-    let chooseSliderPicture = (event) =>{
-        let numberOfSliderBtn = event.target.value
-        if (pictureCounter < numberOfSliderBtn) {
-            let calculatedSliderNumber = numberOfSliderBtn - pictureCounter;
-            setPictureCounter((preValue) => preValue + calculatedSliderNumber)
-        }else if (pictureCounter > numberOfSliderBtn) {
-            let calculatedSliderNumber = pictureCounter - numberOfSliderBtn
-            setPictureCounter((preValue) => preValue - calculatedSliderNumber)
-        }
+    let chooseSliderPicture = (event) => {
+        let numberOfSliderBtn = event.target.value;
+        setPictureCounter(numberOfSliderBtn);
+        setSlideShowPicture(eval(`kep${numberOfSliderBtn}`));
     }
 
-    useEffect(()=> {
+    useEffect(() => {
         let sliderButtons = document.querySelectorAll("#sliderBtn");
-        switch (pictureCounter) {
-            case 1:
-                setSlideShowPicture(kep1)
-                for (let i = 0; i < sliderButtons.length; i++) {
-                    sliderButtons[i].classList.remove("bg-white")
-                }
-                sliderButtons[0].classList.add("bg-white")
-                break;
-            case 2:
-                setSlideShowPicture(kep2)
-                for (let i = 0; i < sliderButtons.length; i++) {
-                    sliderButtons[i].classList.remove("bg-white")
-                }
-                sliderButtons[1].classList.add("bg-white")
-                break;
-            case 3:
-                setSlideShowPicture(kep3)
-                for (let i = 0; i < sliderButtons.length; i++) {
-                    sliderButtons[i].classList.remove("bg-white")
-                }
-                sliderButtons[2].classList.add("bg-white")
-            break;
-            case 4:
-                setSlideShowPicture(kep4)
-                for (let i = 0; i < sliderButtons.length; i++) {
-                    sliderButtons[i].classList.remove("bg-white")
-                }
-                sliderButtons[3].classList.add("bg-white")
-            break;
-            case 5:
-                setSlideShowPicture(kep5)
-                for (let i = 0; i < sliderButtons.length; i++) {
-                    sliderButtons[i].classList.remove("bg-white")
-                }
-                sliderButtons[4].classList.add("bg-white")
-            break;
-            default:
-                setSlideShowPicture(kep1)
-                for (let i = 0; i < sliderButtons.length; i++) {
-                    sliderButtons[i].classList.remove("bg-white")
-                }
-                sliderButtons[0].classList.add("bg-white")
-            break;
-        }
-
-        let pictureCounterIncreaser = setInterval(() =>{
-            if (pictureCounter == 5) {
-                setPictureCounter(1)
-            }else{
-                setPictureCounter((prevPictureCounter) => prevPictureCounter + 1)
+        sliderButtons.forEach((button, index) => {
+            if (index + 1 === pictureCounter) {
+                button.classList.add("bg-white");
+            } else {
+                button.classList.remove("bg-white");
             }
-        },7000)
+        });
 
-        
+        let pictureCounterIncreaser = setInterval(() => {
+            setPictureCounter((prevCounter) => (prevCounter % 5) + 1);
+        }, 7000);
 
-        return () => clearInterval(pictureCounterIncreaser)
+        return () => clearInterval(pictureCounterIncreaser);
+    }, [pictureCounter]);
 
-    },[pictureCounter])
+    // Hírek
+    const adat = [
+        {
+            image: kep1,
+            title: 'Hír1'
+        },
+        {
+            image: kep2,
+            title: 'Hír2'
+        },
+        {
+            image: kep3,
+            title: 'Hír3'
+        },
+    ];
 
-    return(
+    return (
         <>
-            {/*Header*/}
+            {/* Header */}
             <div className="p-5 bg-[#5fbe20] h-32 text-white flex justify-between">
                 <div className="inline-block cursor-pointer"><img className="h-full" src={logo} alt="CsocsoLogo" /></div>
                 <div className="flex items-center justify-around">
@@ -99,21 +102,19 @@ export function WelcomePage() {
                 </div>
             </div>
 
-            {/*Slider*/}
+            {/* Slider */}
             <div className="w-full h-[600px]">
                 <div className="w-full h-full relative">
-                    <img className="h-full w-full" src={slideShowPicture}/>
+                    <img className="h-full w-full cursor-pointer" src={slideShowPicture} />
                     <div className="skibidi flex text-4xl stroke-2 w-24 justify-between absolute bottom-3 left-1/2 -translate-x-1/2 -translate-y-1/2">
-                        <button id="sliderBtn" value={1} onClick={chooseSliderPicture} className="rounded-full w-2 h-2 bg-gray-400 border-solid border-black border-[1px]"></button>
-                        <button id="sliderBtn" value={2} onClick={chooseSliderPicture} className="rounded-full w-2 h-2 bg-gray-400 border-solid border-black border-[1px]"></button>
-                        <button id="sliderBtn" value={3} onClick={chooseSliderPicture} className="rounded-full w-2 h-2 bg-gray-400 border-solid border-black border-[1px]"></button>
-                        <button id="sliderBtn" value={4} onClick={chooseSliderPicture} className="rounded-full w-2 h-2 bg-gray-400 border-solid border-black border-[1px]"></button>
-                        <button id="sliderBtn" value={5} onClick={chooseSliderPicture} className="rounded-full w-2 h-2 bg-gray-400 border-solid border-black border-[1px]"></button>
+                        {Array.from({ length: 5 }, (_, i) => (
+                            <button key={i} id="sliderBtn" value={i + 1} onClick={chooseSliderPicture} className="rounded-full w-2 h-2 bg-gray-400 border-solid border-black border-[1px]"></button>
+                        ))}
                     </div>
                 </div>
             </div>
 
-            {/*About*/}
+            {/* About */}
             <section className="bg-green-100 py-16 px-6 md:px-12 lg:px-24 text-green-900">
                 <div className="max-w-4xl mx-auto text-center">
                     <h2 className="text-4xl font-bold text-green-700 mb-6">Rólunk</h2>
@@ -142,6 +143,30 @@ export function WelcomePage() {
                     </div>
                 </div>
             </section>
+
+            {/* News */}
+            <section className="bg-green-100 py-16 px-6 md:px-12 lg:px-24 text-green-900">
+                <div className="max-w-4xl mx-auto text-center">
+                    <h2 className="text-4xl font-bold text-green-700 mb-6">Hírek</h2>
+                </div>
+            </section>
+            <div className='h-screen bg-green-100 pt-10'>
+                <div className='h-{400px} w-3/4 m-auto '>
+                    <Slider {...SliderSettings}>
+                        {adat.map((item, index) => (
+                            <div key={index} className=" bg-white rounded-2xl shadow-xl shadow-black-500/50 w-80 text-center">
+                                <div className="flex justify-center items-center h-40">
+                                    <img src={item.image} className="w-full h-full object-cover block" />
+                                </div>
+                                <div className="p-10 bg-green-600 rounded-b-lg ">
+                                    <p className="font-semibold text-white">{item.title}</p>
+                                </div>
+                            </div>
+                        ))}
+                    </Slider>
+                </div>
+            </div>
+
         </>
-    )
+    );
 }
