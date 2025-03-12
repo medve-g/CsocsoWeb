abb7f10d30018a9eb14da1c0f58d049cf517ea73
Medve_Gergo <84866190+medve-g@users.noreply.github.com>
2025-03-12 09:56:55 +0000

    Backend created for githubCodeSpaces

--- CsocsoBackend/database/seeders/DatabaseSeeder.php
+++ CsocsoBackend/database/seeders/DatabaseSeeder.php
@@ -4,20 +4,21 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        DB::table("users")->insert([
-            "email" => Str::random(10)."@gmail.com",
-            "password" => Hash::make("password")
-        ]);
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
