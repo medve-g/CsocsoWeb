6efa039a76e9296b443ca7b54d09e54dc3cfe056
vargadavid <vargadavid@gszi.edu.hu>
2025-03-14 09:19:53 +0100

    Nothing important

--- CsocsoFrontend/src/pages/AboutUs.jsx
+++ CsocsoFrontend/src/pages/AboutUs.jsx
@@ -1,7 +1,11 @@
 export function AboutUs(){
     return(
         <>
-        
+        <section id="News-Slider-Box" className="py-16 px-6 md:px-12 lg:px-24 text-green-900">
+                <div className="max-w-4xl mx-auto text-center">
+                    <h2 className="text-4xl font-bold text-green-700 mb-6">Rólunk</h2>
+                </div>
+        </section>
         </>
     )
 }
