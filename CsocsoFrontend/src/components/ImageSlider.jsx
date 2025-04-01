700df088ebebb0cc9b39e7236c42f9512142ae3a
Medve Gergő <medvegergo@gszi.edu.hu>
2025-04-01 11:02:03 +0200

    Failed changes in frontend file

--- CsocsoFrontend/src/components/ImageSlider.jsx
+++ CsocsoFrontend/src/components/ImageSlider.jsx
@@ -7,138 +7,36 @@ import kep5 from "../../public/kep5.jpg";
 import React from "react"
 import { Slide } from "react-slideshow-image";
 import 'react-slideshow-image/dist/styles.css'
-import { useEffect, useState } from "react";
 
 function ImageSlider() {
-
-    const spanStyle = {
-        padding: '20px',
-        background: '#efefef',
-        color: '#000000'
-      }
       
       const divStyle = {
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         backgroundSize: 'cover',
-        height: '400px'
+        height: '400px',
+        width: '100%',
+        backgroundPosition: 'top center'
       }
+
       const slideImages = [
         {
-          url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-          caption: 'Slide 1'
+          url: kep1
         },
         {
-          url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
-          caption: 'Slide 2'
+          url: kep2
         },
         {
-          url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
-          caption: 'Slide 3'
+          url: kep3
+        },
+        {
+          url: kep4
+        },
+        {
+          url: kep5
         },
       ];
-    let [pictureCounter, setPictureCounter] = useState(1);
-
-    // function swipeRight(paramPreviousSlideshowPicture, paramCurrentSlideshowPicture, paramNextSlideshowPicture) {
-    //     let collectImagePlaceholders = [document.getElementById("prevPic"), document.getElementById("currPic"), document.getElementById("nextPic")]
-
-    //     collectImagePlaceholders[2].classList.add("duration-500")
-    //     collectImagePlaceholders[1].classList.add("duration-500")
-    //     collectImagePlaceholders[1].classList.add("-translate-x-full")
-    //     collectImagePlaceholders[2].classList.add("-translate-x-full")
-    //     setTimeout(() => {
-    //         setPreviousSlideshowPicture(paramPreviousSlideshowPicture)
-    //         setCurrentSlideshowPicture(paramCurrentSlideshowPicture)
-    //         setNextSlideshowPicture(paramNextSlideshowPicture)
-    //         collectImagePlaceholders[2].classList.remove("duration-500")
-    //         collectImagePlaceholders[1].classList.remove("duration-500")
-    //         collectImagePlaceholders[2].classList.remove("-translate-x-full")
-    //         collectImagePlaceholders[1].classList.remove("-translate-x-full")
-    //     }, 600)
-
-    // }
-
-    // function selectNextSliderPicture() {
-
-    // }
-
-    // function swipeLeft() {
-    //     if (pictureCounter == 1) {
-    //         setPictureCounter(5)
-    //     } else {
-    //         setPictureCounter((preValue) => preValue - 1)
-    //     }
-    // }
-
-    // function highlightCurrentPictureKnob(knobNumber, sliderButtons) {
-    //     for (let i = 0; i < sliderButtons.length; i++) {
-    //         sliderButtons[i].classList.remove("bg-white")
-    //         sliderButtons[i].classList.add("bg-gray-400")
-    //     }
-    //     sliderButtons[knobNumber - 1].classList.remove("bg-gray-400")
-    //     sliderButtons[knobNumber - 1].classList.add("bg-white")
-    // }
-
-    // let chooseSliderPicture = (event) => {
-    //     let numberOfSliderBtn = event.target.value
-    //     if (pictureCounter < numberOfSliderBtn) {
-    //         let calculatedSliderNumber = numberOfSliderBtn - pictureCounter;
-    //         setPictureCounter((preValue) => preValue + calculatedSliderNumber)
-    //     } else if (pictureCounter > numberOfSliderBtn) {
-    //         let calculatedSliderNumber = pictureCounter - numberOfSliderBtn
-    //         setPictureCounter((preValue) => preValue - calculatedSliderNumber)
-    //     }
-    // }
-
-    // let [firstRun, setFirstRun] = useState(true)
-    // useEffect(() => {
-    //     let sliderButtons = document.querySelectorAll("#sliderBtn");
-    //     switch (pictureCounter) {
-    //         case 1:
-    //             if (firstRun) {
-    //                 setFirstRun(false)
-    //                 highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             } else {
-    //                 swipeRight(kep5, kep1, kep2)
-    //                 highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             }
-    //             break;
-    //         case 2:
-    //             swipeRight(kep1, kep2, kep3)
-    //             highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             break;
-    //         case 3:
-    //             swipeRight(kep2, kep3, kep4)
-    //             highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             break;
-    //         case 4:
-    //             swipeRight(kep3, kep4, kep5)
-    //             highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             break;
-    //         case 5:
-    //             swipeRight(kep4, kep5, kep1)
-    //             highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             break;
-    //         default:
-    //             swipeRight(kep5, kep1, kep2)
-    //             highlightCurrentPictureKnob(pictureCounter, sliderButtons)
-    //             break;
-    //     }
-
-    //     let pictureCounterIncreaser = setInterval(() => {
-    //         if (pictureCounter == 5) {
-    //             setPictureCounter(1)
-    //         } else {
-    //             setPictureCounter((prevPictureCounter) => prevPictureCounter + 1)
-    //         }
-    //     }, 7000)
-
-
-
-    //     return () => clearInterval(pictureCounterIncreaser)
-
-    // }, [pictureCounter])
 
     return (
         <div className="slide-container">
@@ -146,7 +44,6 @@ function ImageSlider() {
          {slideImages.map((slideImage, index)=> (
             <div key={index}>
               <div style={{ ...divStyle, 'backgroundImage': `url(${slideImage.url})` }}>
-                <span style={spanStyle}>{slideImage.caption}</span>
               </div>
             </div>
           ))} 
