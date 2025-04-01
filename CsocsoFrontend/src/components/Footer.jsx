f01613a28d2048d21a544c979dcc669b781b502e
vicziandaniel <vicziandaniel@gszi.edu.hu>
2025-04-01 12:15:00 +0200

    Footer finished

--- CsocsoFrontend/src/components/Footer.jsx
+++ CsocsoFrontend/src/components/Footer.jsx
@@ -1,25 +1,50 @@
 import { faFacebook, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { faPhone, faEnvelope, faHouse } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export function Footer() {
     return (
         <>
             <footer className="bg-[#16a34a] text-center text-white">
-                <p className="text-left text-2xl">Contact us</p>
-                <div class="flex justify-right space-y-10 space-x-10 text-xl text-white">
-                    <a href="#"><FontAwesomeIcon icon={faPhone} />+3620123456789</a>
-                </div>
-                <div className="px-6 pt-6 mx-auto">
-                    <div class="flex justify-center space-x-10 text-2xl text-white">
-                        <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2Fviharsarkicse%2F"><FontAwesomeIcon icon={faFacebook} /></a>
-                        <a href=""><FontAwesomeIcon icon={faLinkedin} /></a>
-                        <a href="https://www.youtube.com/playlist?list=PLBu4v_ZMPewVwk-MmSnFoneDCQfORzpTM"><FontAwesomeIcon icon={faYoutube} /></a>
+                <div class="mx-6 py-10 text-center md:text-left">
+                    <div class="grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-2 place-items-center">
+                        <div class="">
+                            <h6
+                                class="mb-3 flex justify-center font-semibold uppercase md:justify-start">
+                                Contact us
+                            </h6>
+                            <p class="mb-3">
+                                <p class="mb-4">
+                                    <a href="#!"><FontAwesomeIcon icon={faHouse} /> 5600 Békéscsaba, Széchenyi utca 4</a>
+                                </p>
+                                <a href="#"><FontAwesomeIcon icon={faPhone} /> +3620123456789</a>
+                            </p>
+                            <p class="mb-3">
+                                <a href="#!"><FontAwesomeIcon icon={faEnvelope} /> viharsarkicsocso@gmail.com</a>
+                            </p>
+                        </div>
+                        <div>
+                            <h6
+                                class="mb-3 flex justify-center font-semibold uppercase md:justify-start">
+                                Média megjelenéseink
+                            </h6>
+                            <p class="mb-3">
+                                <a href="https://www.facebook.com/login/?next=https%3A%2F%2Fwww.facebook.com%2Fviharsarkicse%2F"><FontAwesomeIcon icon={faFacebook} />Facebook</a>
+                            </p>
+                            <p class="mb-3">
+                                <a href=""><FontAwesomeIcon icon={faLinkedin} />Instagram</a>
+                            </p>
+                            <p class="mb-3">
+                            <a href="https://www.youtube.com/playlist?list=PLBu4v_ZMPewVwk-MmSnFoneDCQfORzpTM"><FontAwesomeIcon icon={faYoutube} />Youtube</a>
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className="p-4 text-center">
-                    &copy; 2025 All rights reserved.
+                <div className="bg-black/5 p-6 text-center">
+                   <a class="font-semibold">&copy; 2025 All rights reserved.</a>
                 </div>
+
+
             </footer>
         </>
     )
