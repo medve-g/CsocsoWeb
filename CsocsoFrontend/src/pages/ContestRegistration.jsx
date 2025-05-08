3f8ec4cb81015e385dd26eef9dfb4e8f9c0c129b
medvegergo <medvegergo@gszi.edu.hu>
2025-05-08 23:57:39 +0200

    REMINDER: we will delete ranklist table and unly use excel fetching for now on, changing on migration file is required

--- CsocsoFrontend/src/pages/ContestRegistration.jsx
+++ CsocsoFrontend/src/pages/ContestRegistration.jsx
@@ -8,7 +8,10 @@ export default function ContestRegistration() {
   const { contestInformation } = location.state || {};
   const [user, setUser] = useContext(UserContext);
   const [registrationData, setRegistrationData] = useState([]);
-  const [categorieReferencesForSpreadsheet, setCategorieReferencesForSpreadsheet] = useState([]);
+  const [
+    categorieReferencesForSpreadsheet,
+    setCategorieReferencesForSpreadsheet,
+  ] = useState([]);
 
   let registrationTester = () => {
     const checkedCategories = document.querySelectorAll(
@@ -18,25 +21,42 @@ export default function ContestRegistration() {
     checkedCategories.forEach((singleCategorie) => {
       let temporaryRegistrationData = {
         registration_submitter: user.id,
-        categorie: singleCategorie.value
-      }
+        categorie: singleCategorie.value,
+      };
     });
 
-    gatherSelectedCategories()
-  }
-
+    fetchExcel();
+  };
 
   let gatherSelectedCategories = async () => {
     let res = await fetch("http://127.0.0.1:8000/api/categories", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(contestInformation.categories),
-    })
-    let data = await res.json()
-    setCategorieReferencesForSpreadsheet(data)
-  }
+    });
+    let data = await res.json();
+    let uniqueReferences = [];
+    let seen = [];
 
-  
+    for (let i = 0; i < data.length; i++) {
+      if (!seen.includes(data[i].ranklist_reference)) {
+        uniqueReferences.push(data[i].ranklist_reference);
+        seen.push(data[i].ranklist_reference);
+      }
+    }
+    setCategorieReferencesForSpreadsheet(uniqueReferences);
+  };
+
+  async function fetchExcel() {
+    let res = await fetch(
+      `https://docs.google.com/spreadsheets/d/1XSWVXtvboY8Rr8sIP0QtDjju4W1fa20u/export?format=xlsx`
+    );
+    let arrayBuffer = await res.arrayBuffer();
+    const workbook = XLSX.read(arrayBuffer, { type: "array" });
+    const sheetName = workbook.SheetNames[i];
+    const worksheet = workbook.Sheets[sheetName];
+    const jsonData = XLSX.utils.sheet_to_json(worksheet);
+  }
 
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -44,22 +64,7 @@ export default function ContestRegistration() {
       setUser(storedUser);
     }
 
-    
-    async function fetchExcel() {
-      let res = await fetch(`https://docs.google.com/spreadsheets/d/1XSWVXtvboY8Rr8sIP0QtDjju4W1fa20u/export?format=xlsx`)
-      let arrayBuffer = await res.arrayBuffer();
-      const workbook = XLSX.read(arrayBuffer, { type: "array" });
-
-      for (let i = 0; i < 5; i++) {
-        const sheetName = workbook.SheetNames[i];
-        const worksheet = workbook.Sheets[sheetName];
-      }
-      const jsonData = XLSX.utils.sheet_to_json(worksheet);
-    }
-
-    async function getherActualCategories() {
-      let res = fetch()
-    }
+    gatherSelectedCategories();
 
     //fetchExcel();
   }, []);
@@ -76,19 +81,23 @@ export default function ContestRegistration() {
               className="flex w-full h-[50px] border-green-400 border-2 my-5"
             >
               <div className="flex justify-center items-center gap-2 text-center px-5">
-                <input className="w-4 h-4" type="checkbox" value={value}/>
+                <input className="w-4 h-4" type="checkbox" value={value} />
                 <p className="text-lg">{key}</p>
               </div>
               <div className="flex justify-center items-center gap-2 text-center px-5">
                 <p className="text-lg">Játékos: </p>
-                <select name="playersFromRanklist" id="playersFromRanklist">
-
-                </select>
+                <select
+                  name="playersFromRanklist"
+                  id="playersFromRanklist"
+                ></select>
               </div>
             </div>
           );
         })}
-        <button onClick={registrationTester} className="bg-green-500 rounded-md font-bold text-white text-medium p-3 border-1 hover:bg-green-600">
+        <button
+          onClick={registrationTester}
+          className="bg-green-500 rounded-md font-bold text-white text-medium p-3 border-1 hover:bg-green-600"
+        >
           Regisztráció
         </button>
       </>
